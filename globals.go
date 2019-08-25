49cb614b6c5e2a686dd598a775a5eee2c1f636d1
Gethiox <gethiox@gmail.com>
2019-08-25 23:17:37 +0200

    Organize constants

--- globals.go
+++ globals.go
@@ -5,29 +5,32 @@ const (
 	CR = byte('\r')
 
 	// Properties
-	PROP_POWER          Property = "power"          // on: smart LED is turned on  /  off: smart LED is turned off
-	PROP_BRIGHT         Property = "bright"         // Brightness percentage. Range 1~100
-	PROP_CT             Property = "ct"             // Color temperature. Range 1700~6500(k)
-	PROP_RGB            Property = "rgb"            // Color. Range 1~16777215
-	PROP_HUE            Property = "hue"            // Hue. Range 0~359
-	PROP_SAT            Property = "sat"            // Saturation. Range 0~100
-	PROP_COLOR_MODE     Property = "color_mode"     // 1: rgb mode / 2: color temperature mode / 3: hsv mode
-	PROP_FLOWING        Property = "flowing"        // 0: no flow is running / 1:color flow is running
-	PROP_DELAYOFF       Property = "delayoff"       // The remaining time of a sleep timer. Range 1~60(minutes)
-	PROP_FLOW_PARAMS    Property = "flow_params"    // Current flow parameters (only meaningful when 'flowing' is 1)
-	PROP_MUSIC_ON       Property = "music_on"       // 1: Music mode is on / 0: Music mode is off
-	PROP_NAME           Property = "name"           // The name of the device set by “set_name” Command
+	PROP_MUSIC_ON    Property = "music_on"    // 1: Music mode is on / 0: Music mode is off
+	PROP_NAME        Property = "name"        // The name of the device set by “set_name” Command
+	PROP_DELAYOFF    Property = "delayoff"    // The remaining time of a sleep timer. Range 1~60(minutes)
+	PROP_ACTIVE_MODE Property = "active_mode" // 0: daylight mode / 1: moonlight mode (ceiling light only)
+
+	PROP_POWER       Property = "power"       // on: smart LED is turned on  /  off: smart LED is turned off
+	PROP_BRIGHT      Property = "bright"      // Brightness percentage. Range 1~100
+	PROP_CT          Property = "ct"          // Color temperature. Range 1700~6500(k)
+	PROP_RGB         Property = "rgb"         // Color. Range 1~16777215
+	PROP_HUE         Property = "hue"         // Hue. Range 0~359
+	PROP_SAT         Property = "sat"         // Saturation. Range 0~100
+	PROP_FLOWING     Property = "flowing"     // 0: no flow is running / 1:color flow is running
+	PROP_FLOW_PARAMS Property = "flow_params" // Current flow parameters (only meaningful when 'flowing' is 1)
+	PROP_COLOR_MODE  Property = "color_mode"  // 1: rgb mode / 2: color temperature mode / 3: hsv mode
+
 	PROP_BG_POWER       Property = "bg_power"       // Background light power status
-	PROP_BG_FLOWING     Property = "bg_flowing"     // Background light is flowing
-	PROP_BG_FLOW_PARAMS Property = "bg_flow_params" // Current flow parametersof background light
-	PROP_BG_CT          Property = "bg_ct"          // Color temperatureof background light
-	PROP_BG_LMODE       Property = "bg_lmode"       // 1: rgb mode / 2: color temperature mode / 3: hsv mode
 	PROP_BG_BRIGHT      Property = "bg_bright"      // Brightness percentageof background light
+	PROP_BG_CT          Property = "bg_ct"          // Color temperatureof background light
 	PROP_BG_RGB         Property = "bg_rgb"         // Colorof background light
 	PROP_BG_HUE         Property = "bg_hue"         // Hueof background light
 	PROP_BG_SAT         Property = "bg_sat"         // Saturationof background light
-	PROP_NL_BR          Property = "nl_br"          // Brightness of night mode light
-	PROP_ACTIVE_MODE    Property = "active_mode"    // 0: daylight mode / 1: moonlight mode (ceiling light only)
+	PROP_BG_FLOWING     Property = "bg_flowing"     // Background light is flowing
+	PROP_BG_FLOW_PARAMS Property = "bg_flow_params" // Current flow parametersof background light
+	PROP_BG_LMODE       Property = "bg_lmode"       // 1: rgb mode / 2: color temperature mode / 3: hsv mode
+
+	PROP_NL_BR Property = "nl_br" // Brightness of night mode light
 
 	CRON_TYPE_POWER_OFF CronType = 0 // power off
 
